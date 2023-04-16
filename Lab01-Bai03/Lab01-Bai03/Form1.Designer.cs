deaa574971de3d1e73012bef8c43f14c8c56cfc5
triledinh159 <91708260+triledinh159@users.noreply.github.com>
2023-04-16 22:09:00 +0700

    second

---
+++ Lab01-Bai03/Lab01-Bai03/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Lab01_Bai03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cdtt = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            cvmon = new TextBox();
+            mon = new TextBox();
+            quydoi = new Label();
+            combo = new ComboBox();
+            combo2 = new ComboBox();
+            del = new Button();
+            exit = new Button();
+            SuspendLayout();
+            // 
+            // cdtt
+            // 
+            cdtt.AutoSize = true;
+            cdtt.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            cdtt.Location = new Point(87, 35);
+            cdtt.Name = "cdtt";
+            cdtt.Size = new Size(233, 32);
+            cdtt.TabIndex = 0;
+            cdtt.Text = "Chuyển đổi tiền tệ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Số tiền cần đổi";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(88, 233);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Số tiền đã đổi";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(88, 319);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Tỷ giá quy đổi";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(88, 152);
+            button1.Name = "button1";
+            button1.Size = new Size(281, 47);
+            button1.TabIndex = 0;
+            button1.Text = "Chuyển đổi";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // cvmon
+            // 
+            cvmon.BorderStyle = BorderStyle.FixedSingle;
+            cvmon.Location = new Point(202, 231);
+            cvmon.Name = "cvmon";
+            cvmon.Size = new Size(167, 27);
+            cvmon.TabIndex = 5;
+            cvmon.TextChanged += cvmon_TextChanged;
+            // 
+            // mon
+            // 
+            mon.BorderStyle = BorderStyle.FixedSingle;
+            mon.Location = new Point(202, 91);
+            mon.Name = "mon";
+            mon.Size = new Size(167, 27);
+            mon.TabIndex = 6;
+            mon.TextChanged += mon_TextChanged;
+            // 
+            // quydoi
+            // 
+            quydoi.AutoSize = true;
+            quydoi.Location = new Point(202, 319);
+            quydoi.Name = "quydoi";
+            quydoi.Size = new Size(0, 20);
+            quydoi.TabIndex = 9;
+            quydoi.Click += quydoi_Click;
+            // 
+            // combo
+            // 
+            combo.FormattingEnabled = true;
+            combo.Location = new Point(449, 90);
+            combo.Name = "combo";
+            combo.Size = new Size(198, 28);
+            combo.TabIndex = 7;
+            combo.SelectedIndexChanged += combo_SelectedIndexChanged;
+            // 
+            // combo2
+            // 
+            combo2.FormattingEnabled = true;
+            combo2.Location = new Point(449, 230);
+            combo2.Name = "combo2";
+            combo2.Size = new Size(198, 28);
+            combo2.TabIndex = 10;
+            combo2.SelectedIndexChanged += combo2_SelectedIndexChanged;
+            // 
+            // del
+            // 
+            del.FlatStyle = FlatStyle.Popup;
+            del.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            del.Location = new Point(449, 152);
+            del.Name = "del";
+            del.Size = new Size(198, 47);
+            del.TabIndex = 11;
+            del.Text = "Xóa";
+            del.UseVisualStyleBackColor = true;
+            del.Click += del_Click;
+            // 
+            // exit
+            // 
+            exit.FlatStyle = FlatStyle.Popup;
+            exit.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            exit.Location = new Point(449, 304);
+            exit.Name = "exit";
+            exit.Size = new Size(198, 47);
+            exit.TabIndex = 12;
+            exit.Text = "Thoát";
+            exit.UseVisualStyleBackColor = true;
+            exit.Click += exit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(exit);
+            Controls.Add(del);
+            Controls.Add(combo2);
+            Controls.Add(quydoi);
+            Controls.Add(combo);
+            Controls.Add(mon);
+            Controls.Add(cvmon);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cdtt);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label cdtt;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private TextBox cvmon;
+        private TextBox mon;
+        private Label quydoi;
+        private ComboBox combo;
+        private ComboBox combo2;
+        private Button del;
+        private Button exit;
+    }
+}
---
+++ Lab01-Bai03/Lab01-Bai03/Form1.cs
@@ -0,0 +1,248 @@
+﻿namespace Lab01_Bai03
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, double> conversionRates;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCurrencyComboBoxes();
+            InitializeConversionRates();
+        }
+        private void InitializeCurrencyComboBoxes()
+        {
+            combo.Items.AddRange(new string[] { "USD", "EUR", "GBP", "SGD", "JPY", "VNĐ" });
+            combo.SelectedIndex = 0;
+
+            combo2.Items.AddRange(new string[] { "USD", "EUR", "GBP", "SGD", "JPY", "VNĐ" });
+            combo2.SelectedIndex = 0;
+        }
+        private void InitializeConversionRates()
+        {
+            conversionRates = new Dictionary<string, double>();
+            conversionRates.Add("USD", 22_772);
+            conversionRates.Add("EUR", 28_132);
+            conversionRates.Add("GBP", 31_538);
+            conversionRates.Add("SGD", 17_286);
+            conversionRates.Add("JPY", 214);
+            conversionRates.Add("VNĐ", 1);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mon_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cvmon_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private double GetExchangeRate(string fromCurrency, string toCurrency)
+        {
+            double rate = 1;
+            switch (fromCurrency.ToUpper())
+            {
+                case "USD":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 1;
+                            break;
+                        case "EUR":
+                            rate = 0.88;
+                            break;
+                        case "GBP":
+                            rate = 0.79;
+                            break;
+                        case "SGD":
+                            rate = 1.37;
+                            break;
+                        case "JPY":
+                            rate = 111.30;
+                            break;
+                        case "VNĐ":
+                            rate = 22772;
+                            break;
+                    }
+                    break;
+                case "EUR":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 1.14;
+                            break;
+                        case "EUR":
+                            rate = 1;
+                            break;
+                        case "GBP":
+                            rate = 0.89;
+                            break;
+                        case "SGD":
+                            rate = 1.54;
+                            break;
+                        case "JPY":
+                            rate = 126.70;
+                            break;
+                        case "VNĐ":
+                            rate = 28132;
+                            break;
+                    }
+                    break;
+                case "GBP":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 1.26;
+                            break;
+                        case "EUR":
+                            rate = 1.12;
+                            break;
+                        case "GBP":
+                            rate = 1;
+                            break;
+                        case "SGD":
+                            rate = 1.73;
+                            break;
+                        case "JPY":
+                            rate = 142.99;
+                            break;
+                        case "VNĐ":
+                            rate = 31538;
+                            break;
+                    }
+                    break;
+                case "SGD":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 0.73;
+                            break;
+                        case "EUR":
+                            rate = 0.65;
+                            break;
+                        case "GBP":
+                            rate = 0.58;
+                            break;
+                        case "SGD":
+                            rate = 1;
+                            break;
+                        case "JPY":
+                            rate = 82.22;
+                            break;
+                        case "VNĐ":
+                            rate = 17286;
+                            break;
+                    }
+                    break;
+                case "JPY":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 0.009;
+                            break;
+                        case "EUR":
+                            rate = 0.008;
+                            break;
+                        case "GBP":
+                            rate = 0.007;
+                            break;
+                        case "SGD":
+                            rate = 0.012;
+                            break;
+                        case "JPY":
+                            rate = 1;
+                            break;
+                        case "VNĐ":
+                            rate = 214;
+                            break;
+                    }
+                    break;
+                case "VNĐ":
+                    switch (toCurrency.ToUpper())
+                    {
+                        case "USD":
+                            rate = 0.009;
+                            break;
+                        case "EUR":
+                            rate = 0.008;
+                            break;
+                        case "GBP":
+                            rate = 0.007;
+                            break;
+                        case "SGD":
+                            rate = 0.012;
+                            break;
+                        case "JPY":
+                            rate = 1;
+                            break;
+                        case "VNĐ":
+                            rate = 1;
+                            break;
+                    }
+                    break;
+            }
+            return rate;
+        }
+
+
+
+        private void combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void combo2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            quydoi.Text = "1 " + combo.SelectedItem.ToString() + " = " + GetExchangeRate(combo.SelectedItem.ToString(), combo2.SelectedItem.ToString()) + " " + combo2.SelectedItem.ToString();
+        }
+        private void ConvertCurrency()
+        {
+            double inputAmount;
+            double.TryParse(mon.Text, out inputAmount);
+
+            double fromRate = conversionRates[combo.SelectedItem.ToString()];
+            double toRate = conversionRates[combo2.SelectedItem.ToString()];
+            quydoi.Text = "1 " + combo.SelectedItem.ToString() + " = " + GetExchangeRate(combo.SelectedItem.ToString(), combo2.SelectedItem.ToString()) + " " + combo2.SelectedItem.ToString();
+
+            double result = inputAmount * fromRate / toRate;
+            cvmon.Text = result.ToString("N0");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void quydoi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            mon.Text = "";
+            cvmon.Text = "";
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
